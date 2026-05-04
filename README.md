8c67ff8036f7ed6816d1010a53bf7e351c514d36
Dildora <48212@edu.raseko.fi>
2026-05-04 09:49:36 +0300

    luotu backend

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # palvelut
+
+- Luodaan Vite frontend project `npm create vite@latest`
+- Luodaan backend `npm init` komennolla
+  Luodaan index.js tiedosto
+- Lisätään package.json scripts:
+
+```js
+{
+  // ...
+  "scripts": {
+
+    "start": "node index.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  // ...
+}
+```
+
+- Asennetaan express `npm install express`
+- Päivätään `npm upgrade`, `npm update`
+- Lisätään app.use(express.json()) middleware, että backend voisi parseroida JSON => JS objekiiin
+
+- middleware varten asennetaan "morgan":
+  `npm install morgan`
+
+Voidaan luoda customtoken Morgan:
+
+```js
+morgan.token("body", (request) => {
+  if (request.method === "POST") {
+    return JSON.stringify(request.body);
+  }
+  return "";
+});
+
+app.use(morgan(":method :url :status :response-time ms :body"));
+```
---
+++ backend/index.js
@@ -0,0 +1,81 @@
+const http = require("http");
+const express = require("express");
+const { log } = require("console");
+const app = express();
+app.use(express.json());
+const morgan = require("morgan");
+
+let services = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+morgan.token("body", (request) => {
+  if (request.method === "POST") {
+    return JSON.stringify(request.body);
+  }
+  return "";
+});
+
+app.use(morgan(":method :url :status :response-time ms :body"));
+
+app.get("/api/services", (request, response) => {
+  response.json(services);
+});
+
+app.get("/api/services/:id", (request, response) => {
+  const id = request.params.id;
+  const service = services.find((s) => s.id === id);
+  if (service) {
+    response.json(service);
+  } else {
+    response.status(404).json({ error: "Service not found" });
+  }
+});
+app.delete("/api/services/:id", (request, response) => {
+  const id = request.params.id;
+  services = services.filter((s) => s.id !== id);
+  response.status(204).end();
+});
+
+const generateId = () => {
+  const randomId =
+    services.length > 0 ? Math.floor(Math.random() * 10000000) : 0;
+  return String(randomId);
+};
+
+app.post("/api/services", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({ error: "Content not exist" });
+  }
+
+  const service = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
+
+  services = services.concat(service);
+
+  response.status(201).json(service);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} `);
+});
